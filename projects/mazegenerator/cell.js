81809c575ac2987bcb4190d689cbbad1a9b0e28e
tutasmaster <ruisims@gmail.com>
2016-07-21 17:31:16 +0100

    Add files via upload

--- projects/mazegenerator/cell.js
+++ projects/mazegenerator/cell.js
@@ -1,25 +1,42 @@
+/************************
+Cell Constructor Function 
+************************/
+
+//The Cell constructor requires an x and a y on the grid
 function Cell(x,y){
+    //Set the position
     this.x = x;
     this.y = y;
 
+    //Set the walls
     this.walls = [true,true,true,true];
 
+    //Set the cell as not visited
     this.visited = false;
 
+    //Display the Cell function
     this.show = function(){
+        
+        //i and j are the Cell position on the canvas
         var i = x * w;
         var j = y * w;
+
+        //Set the fill color vColor(visited Color) if visited and draw a rectangle equivalent to the cell position on the Canvas
         if(this.visited){
             ctx.fillStyle = vColor;
             ctx.fillRect(i,j,w,w);
         }
+        //Set the stroke color as gColor(grid Color)
         ctx.strokeStyle=gColor;
 
+        //Draw a line if the correspondent wall is active
         if(this.walls[0] == true){drawLine(i,j,i+w,j);}
         if(this.walls[1] == true){drawLine(i+w,j,i+w,j+w);}
         if(this.walls[2] == true){drawLine(i+w,j+w,i,j+w);}
         if(this.walls[3] == true){drawLine(i,j+w,i,j);}
     }
+
+    //Highlight the cell function
     this.highlight = function(color){
         var i = this.x*w;
         var j = this.y*w;
@@ -27,14 +44,18 @@ function Cell(x,y){
         ctx.fillRect(i,j,w,w);
     }
 
+    //Check adjacent Cells function
     this.checkNeighbors = function(){
+        //Adjacent cells array
         var neighbors = [];
 
+        //Set the top, right, bottom and left Cells as their own variables
         var top = grid[index(x,y - 1)];
         var right = grid[index(x + 1,y)];
         var bottom = grid[index(x,y + 1)];
         var left = grid[index(x - 1,y)];
 
+        //Push the top, right, bottom and left Cells if they exist and they haven't been visited yet
         if(top && !top.visited){
             neighbors.push(top);
         }
@@ -47,31 +68,54 @@ function Cell(x,y){
         if(left && !left.visited){
             neighbors.push(left);
         }
+
+        //Check if there are available neighbors
         if(neighbors.length > 0){
+            
+            //Get a random number equivalent to one of the adjacent available Cells
             var r = Math.floor(Math.random() * neighbors.length) + 0;
+            
+            //Return the random Cell
             return neighbors[r];
         }else{
+
+            //Otherwise, return undefined
             return undefined;
         }
     }
 }
 
-function removeWalls(a,b){
-  var x = a.x - b.x;
-  if(x === 1){
-    a.walls[3] = false;
-    b.walls[1] = false;
-  }else if(x === -1){
-    a.walls[1] = false;
-    b.walls[3] = false;
-  }
+/*************************
+Remove grid walls function
+*************************/
 
-  var y = a.y - b.y;
-  if(y === 1){
-    a.walls[0] = false;
-    b.walls[2] = false;
-  }else if(y === -1){
-    a.walls[2] = false;
-    b.walls[0] = false;
-  }
+//removeWalls uses 2 Cells as variables
+function removeWalls(a,b){
+
+    //Get which cell is on the left and which one is on the right
+    var x = a.x - b.x;
+
+    //Check which one is on the left and which one is on the right
+    if(x === 1){
+        //Remove the equivalent walls
+        a.walls[3] = false;
+        b.walls[1] = false;
+    }else if(x === -1){
+        //Remove the equivalent walls
+        a.walls[1] = false;
+        b.walls[3] = false;
+    }
+    //Get which cell is on the top and which one is on the bottom
+    var y = a.y - b.y;
+
+    //Check which one is on the top and which one is on the bottom
+    if(y === 1){
+        //Remove the equivalent walls
+        a.walls[0] = false;
+        b.walls[2] = false;
+    }else if(y === -1){
+        //Remove the equivalent walls
+        a.walls[2] = false;
+        b.walls[0] = false;
+    }
 }
--- projects/mazegenerator/setup.js
+++ projects/mazegenerator/setup.js
@@ -1,3 +1,7 @@
+/***************
+Global Variables
+***************/
+
 //Canvas Variables
 var entranceExit = false;
 var HEIGHT = 400;
@@ -13,30 +17,40 @@ var grid = [];
 var current;
 var stack = [];
 
-//Maze Color
-var vColor = "#FFFFFF";
-var gColor = "#000000";
-var nColor = "#000000";
-var cColor = "#FF00FF";
+//Maze Colors
+var vColor = "#FFFFFF"; /*Visited Color*/
+var gColor = "#000000"; /**Grid Color***/
+var nColor = "#000000"; /***Null Color**/
+var cColor = "#FF00FF"; /*Current Color*/
+
+/************************
+Setup the maze generation
+************************/
 
 function setup(){
+  //Clear the last generation
+  grid = [];
   stack = [];
   clearInterval(INTERVAL);
+
+  //Get the display variables through the inputs
   HEIGHT = document.getElementById('height').value;
   WIDTH = document.getElementById("width").value;
   fps = document.getElementById("fps").value;
 
-  grid = [];
-
+  //Get the canvas variable
   canvas = document.getElementById("mazeCanvas");
   ctx = canvas.getContext("2d");
 
+  //Set the height and width of the canvas
   canvas.height = HEIGHT;
   canvas.width = WIDTH;
 
+  //Get the collumns and the rows
   cols = Math.floor(WIDTH/w);
   rows = Math.floor(HEIGHT/w);
 
+  //Create the grid
   for(var j = 0; j < rows; j++){
     for(var i = 0; i < cols; i++){
       var cell = new Cell(i,j);
@@ -44,7 +58,9 @@ function setup(){
     }
   }
 
+  //Set the current cell
   current = grid[0];
 
+  //Set the interval
   var INTERVAL = setInterval(draw,1000/fps);
 }
